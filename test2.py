df1adbf62249df0d45e0bbf5f3b57989c7b9ed26
PAVEL FOMIN <samthesuperhero@users.noreply.github.com>
2018-06-26 01:12:56 +0300

    Upd

--- test2.py
+++ test2.py
@@ -4,10 +4,10 @@ clourfid485.log_enable()
 
 clourfid485.conn_open("/dev/ttyUSB1", 115200, timeout = 1)
 
-clourfid485.send_stop(42, 1)
+print(clourfid485.send_stop(42, 1))
 
 clourfid485.conn_close()
 
 print(clourfid485.get_log())
 
-print("\n\nSuccess!\n")
+print("\nSuccess!\n")
