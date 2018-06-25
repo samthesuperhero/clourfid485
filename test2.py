288d8bcbcaf011855e14b4e3b5a99d1e0f1be606
PAVEL FOMIN <samthesuperhero@users.noreply.github.com>
2018-06-26 01:06:54 +0300

    Type fixed

--- test2.py
+++ test2.py
@@ -1,13 +1,13 @@
-import cloufrid485
+import clourfid485
 
-cloufrid485.log_enable()
+clourfid485.log_enable()
 
-cloufrid485.conn_open("/dev/ttyUSB1", 115200, timeout = 1)
+clourfid485.conn_open("/dev/ttyUSB1", 115200, timeout = 1)
 
-cloufrid485.send_stop(42, 1)
+clourfid485.send_stop(42, 1)
 
-cloufrid485.conn_close()
+clourfid485.conn_close()
 
-print(cloufrid485.get_log())
+print(clourfid485.get_log())
 
 print("\n\nSuccess!\n")
