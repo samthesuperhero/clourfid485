c2fdefc482135bd7e6ec6074c76365537cb8b8d6
PAVEL FOMIN <samthesuperhero@users.noreply.github.com>
2018-06-27 12:38:48 +0300

    Upd

--- test2.py
+++ test2.py
@@ -1,20 +1,20 @@
 import clourfid485
 
-sr_cont = clourfid485.SerialConnectionContext()
+reder_inst_1 = clourfid485.SerialConnectionContext()
+reder_inst_2 = clourfid485.SerialConnectionContext()
 
 clourfid485.log_enable()
 
-clourfid485.conn_open(sr_cont, "/dev/ttyUSB1", 115200, timeout = 1)
+print("1 o ", reder_inst_1.conn_open("/dev/ttyUSB1", 115200, timeout = 1))
 
-print(clourfid485.send_stop(sr_cont, 42))
+print("2 o ", reder_inst_2.conn_open("/dev/ttyUSB1", 115200, timeout = 1))
 
-print(clourfid485.send_stop(sr_cont, 42))
+# print(clourfid485.send_stop(sr_cont, 42))
 
-print(clourfid485.send_stop(sr_cont, 42))
+print("2 c ", reder_inst_2.conn_close())
 
-print(clourfid485.send_stop(sr_cont, 42))
+print("1 c ", reder_inst_1.conn_close())
 
-clourfid485.conn_close(sr_cont)
 
 print(clourfid485.get_log())
 
